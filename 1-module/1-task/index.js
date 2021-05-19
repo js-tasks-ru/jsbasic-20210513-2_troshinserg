4ec97315cc6d9d09e327b891c25e5f70c3fa8d14
Troshin Sergey <46067142+Hudson8811@users.noreply.github.com>
2021-05-19 16:43:17 +0300

    добавляет функцию factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,17 @@
 function factorial(n) {
-  // ваш код...
+  if (!isFinite(n)) {
+    return false;
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  let result = n;
+
+  for(let i = n; i > 1; i--) {
+    result *= i - 1;
+  }
+
+  return result;
 }
