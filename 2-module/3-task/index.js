c43555b711d23b94b909a8e8091071ea7c333dd4
Troshin Sergey <serge-troshin@yandex.ru>
2021-05-22 20:21:02 +0300

    Добавляет calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum(a, b) {
+    return this.a + this.b || false;
+  },
+  mul(a, b) {
+    return this.a * this.b || false;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
