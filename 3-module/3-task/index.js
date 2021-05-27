82622bb5049941fced20e5f5a87b35cd0dbece4d
Troshin Sergey <serge-troshin@yandex.ru>
2021-05-27 22:15:45 +0300

    Добавляет функцию camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  arr.forEach((it, index, arr) => {
+  	if (index > 0) {
+    	arr[index] = it[0].toUpperCase() + it.slice(1);
+    }
+  });
+  return arr.join('');
 }
