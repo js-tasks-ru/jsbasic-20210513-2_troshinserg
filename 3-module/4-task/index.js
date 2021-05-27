09abccb3101b5c2c8f29d324a63b95fc51f17c46
Troshin Sergey <serge-troshin@yandex.ru>
2021-05-27 22:19:10 +0300

    Добавляет функцию showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filteredUsers = users.filter(it => it.age <= age);
+  let result = '';
+  filteredUsers.forEach((it, index, array) => {
+  	result += index < array.length - 1 ? `${it.name}, ${it.balance}\n` : `${it.name}, ${it.balance}`;
+
+  });
+  return result;
 }
