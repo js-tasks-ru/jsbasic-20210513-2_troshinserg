adf03ab8aee7496ea7f82c044c847b238d9f34bc
Troshin Sergey <serge-troshin@yandex.ru>
2021-05-30 16:20:29 +0300

    Добавляет функцию makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('ul');
+
+  friends.forEach(it => {
+  	let li = document.createElement('li');
+    li.textContent = `${it.firstName} ${it.lastName}`;
+  	list.append(li);
+  });
+  return list;
 }
