c15f1f30704e8fb545013570cd61bf10fd27e488
Troshin Sergey <serge-troshin@yandex.ru>
2021-06-10 08:45:01 +0300

    Добавляет класс UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,41 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.render(rows);
+    this.elem.onclick = this.onButtonClick;
+  }
+  render(rows) {
+    const items = rows.map(it => {
+      return `
+      <tr>
+        <td>${it.name}</td>
+        <td>${it.age}</td>
+        <td>${it.salary}</td>
+        <td>${it.city}</td>
+        <td><button type="button">[X]</button></td>
+      </tr>`;
+    }).join('');
+
+    this.elem.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+       ${items}
+      </tbody>`;
+  }
+  onButtonClick(evt) {
+    const target = evt.target;
+
+    if (target === target.closest('button')) {
+      target.closest('tr').remove();
+    }
   }
 }
